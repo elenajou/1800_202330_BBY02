e6e20f913d95ab0a21dfa8577f82d273268f5206
Elena Jou <elenajou.ej@gmail.com>
2023-10-30 16:38:37 -0700

    created the head and footer components and added the base template for fridge and recipe page

---
+++ components/footer.js
@@ -0,0 +1,36 @@
+const footer = document.createElement('template');
+
+footer.innerHTML = `
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="row">
+            <div class="col">
+                <h4>GrocerEase</h4>
+                <div class="social-links">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col">
+                <h4>About</h4>
+                <ul>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Terms and Conditions</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>Connect</h4>
+                <ul>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">(123) 456-789</a></li>
+                <li><a href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+`;
+
+document.body.appendChild(footer.content);
--- components/head.js
+++ components/head.js
@@ -1,8 +1,21 @@
-const template = document.createElement('template');
+const bootstrap_head = document.createElement('template');
 
-template.innerHTML = `
-  <h1>Hello, World!</h1>
-  <p>And all who inhabit it</p>
+bootstrap_head.innerHTML = `
+  <!-- Bootstrap Library CSS CDN go here -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  <!-- Boostrap Library JS CDN  go here -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" //
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+  <!-- Other libraries go here -->
+  <link href="https://fonts.googleapis.com/css2?family=Autour+One&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Autour+One&family=Dosis:wght@500&display=swap" rel="stylesheet">
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 `;
 
-document.body.appendChild(template.content);
+document.head.appendChild(bootstrap_head.content);
