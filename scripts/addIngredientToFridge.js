ba6e8ad6f09804980fc15c5892cc580792d9a2ad
Elena Jou <elenajou@Elenas-MacBook-Air.local>
2023-11-13 17:52:06 -0800

    Created fridge.html and populated ingredients

---
+++ scripts/addIngredientToFridge.js
@@ -0,0 +1,23 @@
+function addIngredientToFridge(ingredient) {
+    var ingredientName = ingredient.toLowerCase();
+    var userRef = db.collection("users");
+    userRef.doc('yzc3UAmLrESJEuWplVCE') // for now this is a specific user "Stella Hui"
+      .get()
+      .then(function(doc) {
+        fridgeRef = userRef.doc(doc.id).collection("fridge");
+        ingredientRef = fridgeRef.doc(ingredientName).get().then(ingredientSnapshot => {
+          if (ingredientSnapshot.exists) {
+            console.log(ingredientName + " exists in the fridge");
+            fridgeRef.doc(ingredientName).update({ qty: firebase.firestore.FieldValue.increment(1) });
+          } else {
+            console.log(ingredientName + "does not exist yet");
+            fridgeRef.doc(ingredientName).set({
+              name: ingredientName,
+              qty: 1
+            })
+          }
+        });
+      });
+  }
+  
+  addIngredientToFridge();
--- scripts/populateUsers.js
+++ scripts/populateUsers.js
@@ -24,8 +24,6 @@ function writeUsers() {
 // writeUsers();
 
 function writeGroceryList() {
-  var recipesRef = db.collection("users").doc();
-  // doublecheck: is your collection called "Reviews" or "reviews"?
   db.collection( "users" )
     .get()
     .then(function(querySnapshot) {
@@ -59,3 +57,74 @@ function writeGroceryList() {
 }
 
 // writeGroceryList();
+
+// function writeFridge() {
+//   db.collection( "users" )
+//     .get()
+//     .then(function(querySnapshot) {
+//       querySnapshot.forEach(function(doc) {
+//         var groceryListRef = db.collection("users").doc(doc.id).collection("fridge");
+//         groceryListRef.add({
+//           code: "ingredient01",
+//           name: "apple",
+//           qty: 2,
+//           last_updated: firebase.firestore.FieldValue.serverTimestamp()
+//         });
+
+//         groceryListRef.add({
+//           code: "ingredient02",
+//           name: "egg",
+//           qty: 2,
+//           last_updated: firebase.firestore.FieldValue.serverTimestamp()
+//         });
+
+//         groceryListRef.add({
+//           code: "ingredient03",
+//           name: "milk",
+//           qty: 1,
+//           last_updated: firebase.firestore.FieldValue.serverTimestamp()
+//         });
+
+//         groceryListRef.add({
+//           code: "ingredient04",
+//           name: "chocolate",
+//           qty: 1,
+//           last_updated: firebase.firestore.FieldValue.serverTimestamp()
+//         });
+
+//         groceryListRef.add({
+//           code: "ingredient05",
+//           name: "mayonaise",
+//           qty: 1,
+//           last_updated: firebase.firestore.FieldValue.serverTimestamp()
+//         });
+        
+//         // doc.data() is never undefined for query doc snapshots
+//         console.log(doc.id, " => ", groceryListRef);
+//     });
+//   });
+// }
+// writeFridge();
+
+function writeFridge() {
+  var userRef = db.collection("users");
+  userRef.doc('yrx60kXc7EuhjYKXrSfC')
+    .get()
+    .then(function(doc) {
+      fridgeRef = userRef.doc(doc.id).collection("fridge");
+      ingredientRef = fridgeRef.doc("egg").get().then(ingredientSnapshot => {
+        if (ingredientSnapshot.exists) {
+          console.log("it exists");
+          fridgeRef.doc("egg").update({ qty: firebase.firestore.FieldValue.increment(1) });
+        } else {
+          console.log("does not exist");
+          fridgeRef.doc("egg").set({
+            name: "egg",
+            qty: 1
+          })
+        }
+      });
+    });
+}
+
+// writeFridge();
