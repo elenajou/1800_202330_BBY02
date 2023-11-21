875fb34801c9bb57db31b8a798f54f62b2bfa4b8
Elena Jou <elenajou@Elenas-MacBook-Air.local>
2023-11-21 00:41:02 -0800

    Completed eachGroceryList

--- scripts/addToGrocerylist.js
+++ scripts/addToGrocerylist.js
@@ -1,57 +1,39 @@
 function addToGroceryList() {
-  // get the recipeID from the URL
-  let params = new URL( window.location.href ); //get URL of search bar
-  let docID = params.searchParams.get( "docID" )
-  var currentUser;
+  const urlParams = new URL(window.location.href);
+  const recipeID = urlParams.searchParams.get("docID");
 
   firebase.auth().onAuthStateChanged(user => {
-    // Check if user is signed in:
-    if (!(user)) {
-      // No user is signed in.
-      console.log ("No user is signed in");
+    if (!user) {
+      console.log("No user is signed in");
     } else {
-      //go to the correct user document by referencing to the user uid
-      currentUser = db.collection("users").doc(user.uid)
-      //get the document for current user.
-      currentUser
-        .get()
-        .then( userDoc => {
-          // grab existing list otherwise set var to empty array
-          var userGroceryList = userDoc.data().groceryList || [];
+      const currentUser = db.collection("users").doc(user.uid);
 
-          // create a new recipe key value pair
-          const addRecipe = {
-            recipeID: db.doc('recipes/' + docID),
-            qty: 1
-          }
-          
-          // Check if the recipeID already exists in userGroceryList
-          // Using Deep Comparison lodal to compare Firestore document reference
-          const existingRecipe = userGroceryList.find(item => _.isEqual(item.recipeID.id, addRecipe.recipeID.id));
+      currentUser.get().then(userDoc => {
+        const userGroceryList = userDoc.data().groceryList || [];
+        const addRecipe = {
+          recipeID: db.doc(`recipes/${recipeID}`),
+          qty: 1
+        };
 
-          if (existingRecipe) {
-            // If the recipe already exists in the list, increment the qty property
-            existingRecipe.qty += 1;
-            
-            // Find the index of the document reference in the array
-            const index = userGroceryList.findIndex(item => _.isEqual(item.recipeID.id, existingRecipe.recipeID.id));
+        const existingRecipeIndex = userGroceryList.findIndex(item =>
+          _.isEqual(item.recipeID.id, addRecipe.recipeID.id)
+        );
 
-            // Update the qty in Firestore
-            if (index != -1) {
-              userGroceryList[index].qty = existingRecipe.qty;
-              // Update the qty property of the specific document reference in the array
-              currentUser
-                .update({ groceryList: userGroceryList })
-                .then(() => { console.log('Qty updated in Firestore successfully'); })
-                .catch(error => { console.error('Error updating qty in Firestore:', error); });
-            }
-          } else {
-            currentUser
-              .update({ groceryList: firebase.firestore.FieldValue.arrayUnion(addRecipe) })
-              .then(console.log("Added to grocery list"))
-              .catch(error => { console.error('Error adding recipe to Firestore:', error); });
-          }
-        });
+        if (existingRecipeIndex !== -1) {
+          // Recipe exists, update qty
+          userGroceryList[existingRecipeIndex].qty += 1;
+          currentUser
+            .update({ groceryList: userGroceryList })
+            .then(() => console.log('Qty updated in Firestore successfully'))
+            .catch(error => console.error('Error updating qty in Firestore:', error));
+        } else {
+          // Recipe doesn't exist, add to the list
+          currentUser
+            .update({ groceryList: firebase.firestore.FieldValue.arrayUnion(addRecipe) })
+            .then(() => console.log('Added to grocery list'))
+            .catch(error => console.error('Error adding recipe to Firestore:', error));
+        }
+      });
     }
   });
 }
--- scripts/eachGroceryList.js
+++ scripts/eachGroceryList.js
@@ -1,81 +1,155 @@
-function displayGroceryItems() {
-  let ingredientList = document.getElementById("ingredients-go-here");
-  let cardTemplate = document.getElementById("ingredientCardTemplate");
+// function displayGroceryItems() {
+//   let ingredientList = document.getElementById("ingredients-go-here");
+//   let cardTemplate = document.getElementById("ingredientCardTemplate");
 
-  firebase.auth().onAuthStateChanged(user => {
-    // Check if user is signed in:
-    if (!(user)) {
-      // No user is signed in.
-      console.log ("No user is signed in");
-    } else {
-      //go to the correct user document by referencing to the user uid
-      const currentUser = db.collection("users").doc(user.uid)
-      
-      //get the document for current user.
-      currentUser
-        .get()
-        .then( userDoc => {
-          var userGroceryList = userDoc.data().groceryList || [];
-          // Create a map to store grouped grocery items by ingredient ID
-          const groupedIngredients = new Map();
+//   firebase.auth().onAuthStateChanged(user => {
+//     // Check if user is signed in:
+//     if (!(user)) {
+//       // No user is signed in.
+//       console.log ("No user is signed in");
+//     } else {
+//       //go to the correct user document by referencing to the user uid
+//       const currentUser = db.collection("users").doc(user.uid)
+
+//       // Create a map to store grouped grocery items by ingredient ID
+//       const groupedIngredients = new Map();
+
+//       //get the document for current user.
+//       currentUser
+//         .get()
+//         .then( userDoc => {
+//           var userGroceryList = userDoc.data().groceryList || [];
           
-          if (userGroceryList) {
-            console.log("found a grocery list!");
+//           if (userGroceryList) {
+//             console.log("found a grocery list!");
   
-            // Use map to track promises
-            const promisesMap = userGroceryList.map(recipe => {
-              const { recipeID } = recipe;
-              const recQty = recipe.qty;
+//             // Use map to track promises
+//             const promises = userGroceryList.flatMap(recipe => {
+//               const { recipeID } = recipe;
+//               const recQty = recipe.qty;
   
-              // ingredientID points to a firestore object referencing an ingredients document
-              return recipeID.get().then(recDoc => {
-                thisRecipe = recDoc.data();
-                const ingredients = recDoc.data().ingredients;
+//               // ingredientID points to a firestore object referencing an ingredients document
+//               return recipeID.get().then(recDoc => {
+//                 thisRecipe = recDoc.data();
+//                 const ingredients = recDoc.data().ingredients;
   
-                // Use Promise.all to wait for all ingredient promises to resolve
-                return Promise.all(
-                  ingredients.map(ingredient => {
-                    const { ingredientID } = ingredient;
-                    const ingQty = ingredient.qty;
+//                 // Use Promise.all to wait for all ingredient promises to resolve
+//                 ingredients.map(ingredient => {
+//                     const { ingredientID } = ingredient;
+//                     const ingQty = ingredient.qty;
   
-                    return ingredientID.get().then(doc => {
-                      const key = doc.id;
-                      if (groupedIngredients.has(key)) {
-                        // If it exists, update the quantity
-                        groupedIngredients.get(key).qty += recQty + ingQty;
-                      } else {
-                        // If it doesn't exist, create a new entry in the map
-                        groupedIngredients.set(key, {
-                          qty: ingQty,
-                          name: doc.data().name,
-                        });
-                      }
-                    });
-                  })
-                );
-              });
-            });
+//                     return ingredientID.get().then(doc => {
+//                       const key = doc.id;
+//                       if (groupedIngredients.has(key)) {
+//                         // If it exists, update the quantity
+//                         groupedIngredients.get(key).qty += recQty + ingQty;
+//                       } else {
+//                         // If it doesn't exist, create a new entry in the map
+//                         groupedIngredients.set(key, {
+//                           qty: ingQty,
+//                           name: doc.data().name,
+//                         });
+//                       }
+//                     });
+//                   })
+//                   // Return a promise that resolves when all ingredient promises are done
+//                   return Promise.all(ingredientPromises);
+//               });
+//             });
+//             // Wait for all recipe promises to resolve
+//             return Promise.all(recipePromises);
+//           } else {
+//             console.log("No Grocery Lists");
+//             return Promise.resolve(); // Resolve with an empty promise if no grocery list
+//           }
+//         })
+//         .then(() => {
+//           // Iterate through the grouped ingredients and create cards
+//           groupedIngredients.forEach((groupedIngredient, ingredientID) => {
+//             console.log(groupedIngredient);
+//             let newCard = cardTemplate.content.cloneNode(true);
   
-            // Use Promise.all to wait for all recipe promises to resolve
-            Promise.all(promisesMap.flat()).then(() => {
-              // Iterate through the grouped ingredients and create cards
-              groupedIngredients.forEach((groupedIngredient, ingredientID) => {
-                console.log(groupedIngredient);
-                let newCard = cardTemplate.content.cloneNode(true);
+//             newCard.querySelector('.card-image').src = `/images/recipe01.jpg`;
+//             newCard.querySelector('.card-title').innerHTML =
+//               groupedIngredient.qty + " &times " + groupedIngredient.name;
   
-                newCard.querySelector('.card-image').src = `/images/recipe01.jpg`;
-                newCard.querySelector('.card-title').innerHTML =
-                  groupedIngredient.qty + " &times " + groupedIngredient.name;
-  
-                ingredientList.appendChild(newCard);
-              });
-            });
+//             ingredientList.appendChild(newCard);
+//           });
+//         })
+//         .catch(error => {
+//           console.error('Error:', error);
+//         });
+//     }
+//   });
+// }
+
+// displayGroceryItems();
+function displayGroceryItems() {
+  const ingredientList = document.getElementById("ingredients-go-here");
+  const cardTemplate = document.getElementById("ingredientCardTemplate");
+
+  firebase.auth().onAuthStateChanged(async user => {
+    try {
+      if (!user) {
+        console.log("No user is signed in");
+        return;
+      }
+
+      const currentUser = db.collection("users").doc(user.uid);
+      const userDoc = await currentUser.get();
+      const userGroceryList = userDoc.data().groceryList || [];
+      const groupedIngredients = new Map();
+
+      if (userGroceryList.length === 0) {
+        console.log("No Grocery Lists");
+        return;
+      }
+
+      console.log("Found a grocery list!");
+
+      for (const recipe of userGroceryList) {
+        const { recipeID } = recipe;
+        const recQty = recipe.qty;
+        const recDoc = await recipeID.get();
+        const ingredients = recDoc.data().ingredients;
+
+        for (const ingredient of ingredients) {
+          const { ingredientID } = ingredient;
+          const ingQty = ingredient.qty;
+          const doc = await ingredientID.get();
+
+          const key = doc.id;
+          if (groupedIngredients.has(key)) {
+            groupedIngredients.get(key).qty += recQty * ingQty;
           } else {
-            console.log("No Grocery Lists");
+            groupedIngredients.set(key, {
+              qty: recQty * ingQty,
+              name: doc.data().name,
+            });
           }
-        })
+        }
+      }
+
+      // Now, groupedIngredients should be populated correctly
+      console.log(groupedIngredients);
+
+      // Iterate through the grouped ingredients and create cards
+      ingredientList.innerHTML = ''; // Clear existing content
+      groupedIngredients.forEach((groupedIngredient, ingredientID) => {
+        console.log(groupedIngredient);
+        const newCard = cardTemplate.content.cloneNode(true);
+
+        newCard.querySelector('.card-image').src = `/images/recipe01.jpg`;
+        newCard.querySelector('.card-title').innerHTML =
+          groupedIngredient.qty + " &times " + groupedIngredient.name;
+
+        ingredientList.appendChild(newCard);
+      });
+    } catch (error) {
+      console.error('Error:', error);
     }
   });
 }
 
+// Call the function to start listening for changes
 displayGroceryItems();
