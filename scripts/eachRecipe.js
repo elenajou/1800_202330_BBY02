99ee30aaa2be3bd3c5c05cf2594810041a4e1a13
Stella Hui <stellasylviahui@hotmail.com>
2023-11-21 11:31:44 -0800

    Created bookmark button for eachRecipe.html

--- scripts/eachRecipe.js
+++ scripts/eachRecipe.js
@@ -1,40 +1,97 @@
-function displayRecipeInfo() {
+function doAll() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      currentUser = db.collection("users").doc(user.uid); //global
+      console.log(currentUser);
 
-  let params = new URL( window.location.href ); //get URL of search bar
-  let ID = params.searchParams.get( "docID" ); //get value for key "id"
+      currentUser.get().then((userDoc) => {
+        var bookmarks = userDoc.data().bookmarks || [];
+        updateBookmarkIcon(bookmarks);
+      });
+    } else {
+      console.log("No user is signed in");
+      window.location.href = "login.html";
+    }
+  });
+}
+
+function updateBookmarkIcon(bookmarks) {
+  bookmarks.forEach((recipeID) => {
+    var iconID = "save-" + recipeID;
+    var iconElement = document.getElementById(iconID);
+    if (iconElement) {
+      iconElement.innerHTML = "bookmark";
+    }
+  });
+}
+
+doAll();
+
+function displayRecipeInfo() {
+  let params = new URL(window.location.href); //get URL of search bar
+  let ID = params.searchParams.get("docID"); //get value for key "id"
 
   var recipeRef = db.collection("recipes").doc(ID);
 
-  recipeRef
-    .get()
-    .then( doc => {
-      thisRecipe = doc.data();
-      console.log(thisRecipe.name);
-      recipeCode = thisRecipe.code;
-      recipeName = thisRecipe.name;
-      recipeDesc = thisRecipe.description;
-      recipeInstr = thisRecipe.instructions;
-      recipeIngre = thisRecipe.ingredients;
-      
-      document.getElementById( "recipeName" ).innerHTML = recipeName;
-      document.getElementById( "recipeDesc" ).innerHTML = recipeDesc;
-      document.getElementById( "recipeInstr" ).innerHTML = recipeInstr;
-      
-      let ingredientList = document.getElementById( "recipeIngre" );
+  recipeRef.get().then((doc) => {
+    thisRecipe = doc.data();
+    console.log(thisRecipe.name);
+    recipeCode = thisRecipe.code;
+    recipeName = thisRecipe.name;
+    recipeDesc = thisRecipe.description;
+    recipeInstr = thisRecipe.instructions;
+    recipeIngre = thisRecipe.ingredients;
 
-      // Iterates through the ingredients array and maps out each item
-      recipeIngre.forEach( ingredient => {
-        const { ingredientID, qty } = ingredient;
-        // ingredientID points to a firestore object referencing an ingredients document
-        ingredientID
-          .get()
-          .then( doc => {
-            let listItem = document.createElement('li');
-            listItem.innerHTML = doc.data().name + ": " + qty;
-            ingredientList.appendChild(listItem);
-          })
+    document.getElementById("recipeName").innerHTML = recipeName;
+    document.getElementById("recipeDesc").innerHTML = recipeDesc;
+    document.getElementById("recipeInstr").innerHTML = recipeInstr;
+
+    let ingredientList = document.getElementById("recipeIngre");
+
+    // Iterates through the ingredients array and maps out each item
+    recipeIngre.forEach((ingredient) => {
+      const { ingredientID, qty } = ingredient;
+      // ingredientID points to a firestore object referencing an ingredients document
+      ingredientID.get().then((doc) => {
+        let listItem = document.createElement("li");
+        listItem.innerHTML = doc.data().name + ": " + qty;
+        ingredientList.appendChild(listItem);
       });
     });
+
+    currentUser.get().then((userDoc) => {
+      var bookmarks = userDoc.data().bookmarks || [];
+      var isBookmarked = bookmarks.includes(ID);
+      let bookmarkButton = document.getElementById("bookmark-button");
+      bookmarkButton.innerHTML = isBookmarked ? "Bookmarked" : "Bookmark";
+    });
+  });
 }
 
 displayRecipeInfo();
+
+function bookmarkRecipe() {
+  // Get the recipe ID from the URL
+  let params = new URL(window.location.href);
+  let recipeID = params.searchParams.get("docID");
+
+  currentUser.get().then((userDoc) => {
+    var bookmarks = userDoc.data().bookmarks || [];
+    var isBookmarked = bookmarks.includes(recipeID);
+
+    // Update the user's bookmarks with the recipe ID
+    currentUser.update({
+      bookmarks: isBookmarked
+        ? firebase.firestore.FieldValue.arrayRemove(recipeID)
+        : firebase.firestore.FieldValue.arrayUnion(recipeID),
+    }).then(() => {
+      console.log("Bookmark has been " + (isBookmarked ? "removed" : "saved") + "for " + recipeID);
+      let bookmarkButton = document.getElementById("bookmark-button");
+      bookmarkButton.innerHTML = isBookmarked ? "Bookmark" : "Bookmarked";
+      }).catch((error) => {
+        console.error("Error bookmarking recipe ", error);
+      });
+  });
+}
+
+document.getElementById("bookmark-button").addEventListener("click", bookmarkRecipe);
--- scripts/recipeMenu.js
+++ scripts/recipeMenu.js
@@ -1,9 +1,12 @@
+var currentUser;
+
 function displayCardsDynamically(collection) {
   let cardTemplate = document.getElementById("recipeCardTemplate");
 
-  db.collection(collection).get()   //the collection called "recipes"
-    .then(allRecipes => {
-      allRecipes.forEach(doc => {
+  db.collection(collection)
+    .get() //the collection called "recipes"
+    .then((allRecipes) => {
+      allRecipes.forEach((doc) => {
         var title = doc.data().name;
         var cookTime = doc.data().cook_time;
         var description = doc.data().description;
@@ -12,76 +15,93 @@ function displayCardsDynamically(collection) {
         var docID = doc.id;
 
         // update title and text and image
-        newCard.querySelector('.card-title').innerHTML = title;
-        newCard.querySelector('.card-time').innerHTML = "Total time: " + cookTime + "mins";
-        newCard.querySelector('.card-text').innerHTML = description.slice(0, 80) + "...";
+        newCard.querySelector(".card-title").innerHTML = title;
+        newCard.querySelector(".card-time").innerHTML =
+          "Total time: " + cookTime + "mins";
+        newCard.querySelector(".card-text").innerHTML =
+          description.slice(0, 80) + "...";
         // newCard.querySelector('.card-image').src = `../images/${recipeCode}.jpg`;
-        newCard.querySelector('a').href = "eachRecipe.html?docID=" + docID;
-        newCard.querySelector('i').id = 'save-' + docID;
-        newCard.querySelector('i').onclick = () => saveBookmark(docID); 
-        
+        newCard.querySelector("a").href = "eachRecipe.html?docID=" + docID;
+        newCard.querySelector("i").id = "save-" + docID;
+        newCard.querySelector("i").onclick = () => saveBookmark(docID);
+
         // get image URL from FireBase Storage
         const storage = firebase.storage();
         var imageRef = storage.ref(recipeCode + ".jpg");
-        imageRef.getDownloadURL().then(url => {
-          newCard.querySelector('.card-image').src = url;
+        imageRef.getDownloadURL().then((url) => {
+          newCard.querySelector(".card-image").src = url;
         });
 
         // var imageURL = `https://storage.googleapis.com/bby02-grocerease.appspot.com/${recipeCode}.jpg`;
         // newCard.querySelector('.card-image').src = imageURL;
 
-        currentUser.get().then(userDoc => {
+        currentUser.get().then((userDoc) => {
           //get the user name
-          var bookmarks = userDoc.data().bookmarks;
+          var bookmarks = userDoc.data().bookmarks || [];
           if (bookmarks.includes(docID)) {
-            document.getElementById('save-' + docID).innerText = 'bookmark';
+            iconElement.innerHTML = "bookmark";
           }
-        })
-          document.getElementById(collection + "-go-here").appendChild(newCard);
-        })
-      })
+        });
+        document.getElementById(collection + "-go-here").appendChild(newCard);
+      });
+    });
+}
+
+function doAll() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      currentUser = db.collection("users").doc(user.uid); //global
+      console.log(currentUser);
+      displayCardsDynamically("recipes");
+
+      currentUser.get().then((userDoc) => {
+        var bookmarks = userDoc.data().bookmarks || [];
+        let params = new URL(window.location.href);
+        let isCurrentlyBookmarked = bookmarks.includes(
+          params.searchParams.get("docID")
+        );
+
+        bookmarks.forEach((recipeID) => {
+          var iconID = "save-" + recipeID;
+          var iconElement = document.getElementById(iconID);
+          let params = new URL(window.location.href);
+          if (iconElement) {
+            iconElement.innerHTML = "bookmark";
+          }
+          if (isCurrentlyBookmarked && params.searchParams.get("docID") === recipeID) {
+            let bookmarkButton = document.getElementById("bookmark-button");
+            bookmarkButton.innerHTML = "Bookmarked";
+          }
+        });
+      });
+    } else {
+      console.log("No user is signed in");
+      window.location.href = "login.html";
     }
-  // displayCardsDynamically("recipes");
+  });
+}
+doAll();
 
-  //Global variable pointing to the current user's Firestore document
-  var currentUser;
+function saveBookmark(recipeDocID) {
+  currentUser.get().then((userDoc) => {
+    var bookmarks = userDoc.data().bookmarks || [];
+    var isBookmarked = bookmarks.includes(recipeDocID);
 
-  //Function that calls everything needed for the main page  
-  function doAll() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        currentUser = db.collection("users").doc(user.uid); //global
-        console.log(currentUser);
-        displayCardsDynamically("recipes");
-      } else {
-        // No user is signed in.
-        console.log("No user is signed in");
-        window.location.href = "login.html";
-      }
-    });
-  }
-  doAll();
-
-  function saveBookmark(recipeDocID) {
-    currentUser.get().then(userDoc => {
-      var bookmarks = userDoc.data().bookmarks || [];
-
-      var isBookmarked = bookmarks.includes(recipeDocID);
-      if (isBookmarked) {
-        currentUser.update({
-          bookmarks: firebase.firestore.FieldValue.arrayRemove(recipeDocID)
-        });
-        console.log("Bookmark has been removed for " + recipeDocID);
-      } else { 
-        currentUser.update ({
-          bookmarks: firebase.firestore.FieldValue.arrayUnion(recipeDocID)
-        });
-        console.log("Bookmark has been saved for " + recipeDocID);
-      }
-      var iconID = 'save-' + recipeDocID;
-      var iconElement = document.getElementById(iconID);
-      if (iconElement) {
-        iconElement.innerHTML = isBookmarked ? 'bookmark_border' : 'bookmark';
-      }
-    });
-  }
+    if (isBookmarked) {
+      currentUser.update({
+        bookmarks: firebase.firestore.FieldValue.arrayRemove(recipeDocID),
+      });
+      console.log("Bookmark has been removed for " + recipeDocID);
+    } else {
+      currentUser.update({
+        bookmarks: firebase.firestore.FieldValue.arrayUnion(recipeDocID),
+      });
+      console.log("Bookmark has been saved for " + recipeDocID);
+    }
+    var iconID = "save-" + recipeDocID;
+    var iconElement = document.getElementById(iconID);
+    if (iconElement) {
+      iconElement.innerHTML = isBookmarked ? "bookmark_border" : "bookmark";
+    }
+  });
+}
