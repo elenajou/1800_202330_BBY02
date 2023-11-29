4446f4e6a865c289d3047afb00f3ee9c31ce147a
Elena Jou <elenajou@Elenas-MacBook-Air.local>
2023-11-28 21:44:00 -0800

    Incorporated newIngredient into createRecipe

--- pages/createRecipe/createRecipe.js
+++ pages/createRecipe/createRecipe.js
@@ -8,7 +8,6 @@ function populateIngredientsMenu(dropdownID) {
       const ingredientsRef = await db.collection("ingredients").orderBy('name').get();
 
       if (!(ingredientsRef) || !dropdownID) return console.log("None");
-      console.log(dropdownID);
 
       ingredientsRef.forEach(ingredientDoc => {
         const dropdown = document.getElementById(dropdownID);
@@ -67,7 +66,7 @@ function createRecipe() {
   const ingredients = document.getElementById("ingredients-dropdowns");
   const ingredientsMenu = ingredients.querySelector(".ingredients-menu");
   const ingredientsQty = ingredients.getElementsByClassName("quantity");
-  const ingredientsArray =[];
+  const ingredientsArray = [];
   
   for (let i = 0; i < ingredientsQty.length; i++) {
     const ingredientID = db.collection('ingredients').doc(ingredientsMenu[i].id);
@@ -89,6 +88,35 @@ function createRecipe() {
   $('#createdRecipe').modal('show');
 }
 
-$(".modal").on("hidden.bs.modal", function () {
-  window.location = "/recipeMenu";
-});
+/* Creates a new ingredient document and adds it to firestore. */
+function createIngredient() {
+  const newIngredientName = document.getElementById("ingredientName").value;
+  const newExpiryDays = parseInt(document.getElementById("expiryDays").value);
+  
+  var ingredientsRef = db.collection("ingredients");
+
+  ingredientsRef.add({
+    name: newIngredientName,
+    expiryDays: newExpiryDays,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+  });
+
+  const msgRef = document.getElementById("newIngredientMsg")
+  msgRef.innerHTML = `${newIngredientName} has been added`;
+  msgRef.classList.remove("hidden");
+  
+  for (let i = 1; i <= ingredientIndex; i++) {
+    const dropdownID = `ingredient-${i}`;
+    const ingredientDropdowns = document.getElementById(dropdownID);
+    ingredientDropdowns.innerHTML = "";
+    populateIngredientsMenu(dropdownID);
+  }
+}
+
+$('#newIngredientModal').on('shown.bs.modal', function () {
+  $('#newIngredient').trigger('focus')
+})
+
+// $(".added-recipe").on("hidden.bs.modal", function () {
+//   window.location = "/recipeMenu";
+// });
